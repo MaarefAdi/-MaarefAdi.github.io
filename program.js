785eed776e0757e8ef77df2ac3eb5ec354a2752e
MaarefAdi <39432388+MaarefAdi@users.noreply.github.com>
2018-05-19 16:44:43 +0100

    Update program.js

--- program.js
+++ program.js
@@ -96,6 +96,18 @@
           id: "ok"
     }
 ];
+// Rigister the Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
