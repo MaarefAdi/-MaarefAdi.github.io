2f8747de458bbbf24cc1b3e36192df0e5d1a11f5
MaarefAdi <39432388+MaarefAdi@users.noreply.github.com>
2018-05-19 16:45:16 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// Files to cache
+var cacheName = 'js13kPWA-v1';
+var appShellFiles = [
+  '/Adi-app/',
+  '/Adi-app/index.html',
+  '/pwa-examples/js13kpwa/app.js',
+  '/Adi-app/style.css',
+  '/Adi-app/js13kgames.png',
+  '/Adi-app/icon-192.png',
+  '/Adi-app/icon-512.png'
+];
+
+// Installing Service Worker
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
