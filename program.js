6de74c46e3dfec594e0230464a152cd21583d056
MaarefAdi <39432388+MaarefAdi@users.noreply.github.com>
2018-05-20 18:08:14 +0100

    Update program.js

--- program.js
+++ program.js
@@ -109,31 +109,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-let btnAdd= document.getElementById('installAppBtn');
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  btnAdd.style.display = 'block';
-});
 
-btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-});
   // fonction pour Dynamiser le tableau des Users :
   function creatUserBox() {
       $("#userBox").html("");
