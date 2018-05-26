627c229e9ad167cec8896f3d192c8f2a28138092
MaarefAdi <39432388+MaarefAdi@users.noreply.github.com>
2018-05-26 14:33:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (!process.env.VAPID_PUBLIC_KEY || !process.env.VAPID_PRIVATE_KEY) {
 // Set the keys used for encrypting the push messages.
 
 webPush.setVapidDetails(
-  'https://maarefadi.github.io/',
+  'https://serviceworke.rs/',
   process.env.VAPID_PUBLIC_KEY,
   process.env.VAPID_PRIVATE_KEY
 );
