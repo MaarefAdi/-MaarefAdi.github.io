93fc9f9def2e48aeae18fff41122c5466f9475b8
MaarefAdi <39432388+MaarefAdi@users.noreply.github.com>
2018-05-26 09:35:59 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,35 +3,30 @@ function getEndpoint() {
   .then(function(subscription) {
     if (subscription) {
       return subscription.endpoint;
+      console.log('User subscribed to notifcation, getting the end point of subscription');
     }
 
     throw new Error('User not subscribed');
   });
 }
-// Register event listener for the ‘push’ event.
 
-
+// Register event listener for the 'push' event.
 self.addEventListener('push', function(event) {
-
-// Keep the service worker alive until the notification is created.
-
-
+  // Keep the service worker alive until the notification is created.
   event.waitUntil(
     getEndpoint()
     .then(function(endpoint) {
-// Retrieve the textual payload from the server using a GET request. We are using the endpoint as an unique ID of the user for simplicity.
-
-
+      // Retrieve the textual payload from the server using a GET request.
+      // We are using the endpoint as an unique ID of the user for simplicity.
       return fetch('./getPayload?endpoint=' + endpoint);
     })
     .then(function(response) {
       return response.text();
     })
     .then(function(payload) {
-// Show a notification with title ‘ServiceWorker Cookbook’ and use the payload as the body.
-
-
-      self.registration.showNotification('PWA Notif', {
+      // Show a notification with title 'ServiceWorker Cookbook' and use the payload
+      // as the body.
+      self.registration.showNotification('ServiceWorker Cookbook', {
         body: payload,
       });
     })
